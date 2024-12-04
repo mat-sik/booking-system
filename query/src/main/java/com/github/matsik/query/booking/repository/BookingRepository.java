7e51cb81eadff7bceb77f01e3fb734db4ea68c25
Mateusz <msikorski@griddynamics.com>
2024-12-04 14:50:55 +0100

    Add initial version of the second query.

--- query/src/main/java/com/github/matsik/query/booking/repository/BookingRepository.java
+++ query/src/main/java/com/github/matsik/query/booking/repository/BookingRepository.java
@@ -1,12 +1,16 @@
 package com.github.matsik.query.booking.repository;
 
+import com.github.matsik.query.booking.model.ServiceBooking;
 import com.github.matsik.query.booking.model.UserBooking;
 import com.github.matsik.query.booking.query.GetBooking;
+import com.github.matsik.query.booking.query.GetBookings;
 import com.github.matsik.query.booking.query.ServiceBookingIdentifier;
 import lombok.RequiredArgsConstructor;
+import org.bson.Document;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Component;
 
@@ -51,6 +55,23 @@ public class BookingRepository {
     private static AggregationOperation getMatchOperation(ServiceBookingIdentifier identifier) {
         return Aggregation.match(Criteria.where("date").is(identifier.date())
                 .and("serviceId").is(identifier.serviceId()));
+    }
+
+    /*
+     * Spring Boot data mongodb doesn't support projections that are more advanced than the most basic of use cases.
+     */
+    public List<ServiceBooking> getBookings(GetBookings request) {
+        Document queryFilter = new Document("date", new Document("$in", request.dates()))
+                .append("serviceId", new Document("$in", request.serviceIds()));
+
+        Document projection = new Document("date", 1).append("serviceId", 1).append("bookings",
+                new Document("$filter",
+                        new Document("input", "$bookings").append("cond",
+                                new Document("$in", List.of("$$this.userId", request.userIds())))));
+
+        BasicQuery query = new BasicQuery(queryFilter, projection);
+
+        return template.find(query, ServiceBooking.class, "service_bookings");
     }
 
 }
--- query/src/test/java/com/github/matsik/query/booking/repository/BookingRepositoryTest.java
+++ query/src/test/java/com/github/matsik/query/booking/repository/BookingRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.github.matsik.query.booking.repository;
 
 import com.github.matsik.query.booking.query.GetBooking;
+import com.github.matsik.query.booking.query.GetBookings;
 import com.github.matsik.query.booking.query.ServiceBookingIdentifier;
+import net.bytebuddy.asm.Advice;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +35,19 @@ class BookingRepositoryTest {
         GetBooking request = new GetBooking(identifier, bookingId);
 
         var out = bookingRepository.getUserBooking(request);
+        System.out.println(out);
+    }
+
+    @Test
+    void getBookings() {
+        List<LocalDate> dates = List.of(
+                LocalDate.of(2024, 12, 3),
+                LocalDate.of(2024, 12, 4)
+        );
+        List<ObjectId> serviceIds = List.of(new ObjectId("aaaaaaaaaaaaaaaaaaaaaaaa"));
+        List<ObjectId> userIds = List.of(new ObjectId("bbbbbbbbbbbbbbbbbbbbbbbb"));
+        GetBookings request = new GetBookings(dates, serviceIds, userIds);
+        var out = bookingRepository.getBookings(request);
         System.out.println(out);
     }
 
