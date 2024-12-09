fc11ce6a530783d905b36c884aaebf8b8dd43c1d
Mateusz <msikorski@griddynamics.com>
2024-12-09 12:49:42 +0100

    Autoformat.

--- booking-service/src/main/java/com/github/matsik/booking/client/kafka/KafkaClientConfiguration.java
+++ booking-service/src/main/java/com/github/matsik/booking/client/kafka/KafkaClientConfiguration.java
@@ -1,27 +1,18 @@
 package com.github.matsik.booking.client.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.matsik.kafka.mapping.LocalDateDeserializer;
 import com.github.matsik.kafka.mapping.LocalDateSerializer;
 import com.github.matsik.kafka.task.CommandValue;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.time.LocalDate;
