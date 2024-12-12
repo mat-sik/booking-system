0b12288d83b653d41b7897589029c0914be71360
Mateusz <msikorski@griddynamics.com>
2024-12-12 16:01:06 +0100

    Autoforamt.

--- query-service/src/main/java/com/github/matsik/query/booking/controller/BookingController.java
+++ query-service/src/main/java/com/github/matsik/query/booking/controller/BookingController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
