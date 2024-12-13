cf4bbaf175e13379d176f23361c04df56343c009
Mateusz <msikorski@griddynamics.com>
2024-12-13 16:10:48 +0100

    Implement the first test of the second method.

--- query-service/src/test/java/com/github/matsik/query/booking/controller/BookingControllerTest.java
+++ query-service/src/test/java/com/github/matsik/query/booking/controller/BookingControllerTest.java
@@ -1,8 +1,12 @@
 package com.github.matsik.query.booking.controller;
 
+import com.fasterxml.jackson.databind.util.JacksonCollectors;
+import com.github.matsik.query.booking.model.UserBooking;
 import com.github.matsik.query.booking.query.GetAvailableTimeRangesQuery;
+import com.github.matsik.query.booking.query.GetBookingQuery;
 import com.github.matsik.query.booking.service.BookingService;
 import com.github.matsik.query.booking.service.TimeRange;
+import com.github.matsik.query.config.jackson.JacksonConfiguration;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,6 +14,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,7 +26,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.aMapWithSize;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(BookingController.class)
+@Import(JacksonConfiguration.class)
 class BookingControllerTest {
 
     @Autowired
@@ -209,8 +214,82 @@ class BookingControllerTest {
         }
     }
 
-    @Test
-    void getUserBooking() {
+    private static Stream<Arguments> provideGetUserBookingTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        "Ok response.",
+                        LocalDate.of(2024, 12, 12).format(DateTimeFormatter.ISO_LOCAL_DATE),
+                        new ObjectId("000000000000000000000000").toHexString(),
+                        new ObjectId("100000000000000000000000").toHexString(),
+                        new UserBooking(
+                                new ObjectId("110000000000000000000000"),
+                                60,
+                                120
+                        ),
+                        (MockMvcExpectationAssertion<UserBooking>) (resultActions, userBooking) -> {
+                            resultActions
+                                    .andExpect(status().isOk())
+                                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                                    .andExpect(jsonPath("$", aMapWithSize(3)))
+                                    .andExpect(jsonPath("$.userId").value("110000000000000000000000"))
+                                    .andExpect(jsonPath("$.start").value(60))
+                                    .andExpect(jsonPath("$.end").value(120));
+                        },
+                        (MockServiceAssertion<GetBookingQuery>) (service, query) -> {
+                            then(service).should().getUserBooking(query);
+                        }
+
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideGetUserBookingTestCases")
+    void getUserBooking(
+            String name,
+            String date,
+            String serviceId,
+            String bookingId,
+            UserBooking userBooking,
+            MockMvcExpectationAssertion<UserBooking> mockMvcExpectationAssertion,
+            MockServiceAssertion<GetBookingQuery> mockServiceAssertion
+    ) throws Exception {
+        // given
+        GetBookingQuery query = getGetBookingQueryOrDefault(date, serviceId, bookingId);
+        when(service.getUserBooking(query))
+                .thenReturn(userBooking);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/booking")
+                .param("date", date)
+                .param("serviceId", serviceId)
+                .param("bookingId", bookingId)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        mockMvcExpectationAssertion.assertExpectations(resultActions, userBooking);
+
+        mockServiceAssertion.assertMock(service, query);
+    }
+
+    private static GetBookingQuery getGetBookingQueryOrDefault(
+            String dateString,
+            String serviceIdString,
+            String bookingIdString
+    ) {
+        try {
+            LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE);
+            ObjectId serviceId = new ObjectId(serviceIdString);
+            ObjectId bookingId = new ObjectId(bookingIdString);
+
+            return GetBookingQuery.Factory.create(date, serviceId, bookingId);
+        } catch (Exception ex) {
+            return GetBookingQuery.Factory.create(
+                    LocalDate.of(2000, 1, 1),
+                    new ObjectId("aaaaaaaaaaaaaaaaaaaaaaaa"),
+                    new ObjectId("bbbbbbbbbbbbbbbbbbbbbbbb")
+            );
+        }
     }
 
     @Test
