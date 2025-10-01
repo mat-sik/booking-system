92e1cdc6afc90ec3532f19e94d7e2c239a2c7b0e
Mateusz <siiikor.sikorski@gmail.com>
2025-10-01 20:56:59 +0200

    Add better test cases

--- query-service/src/main/java/com/github/matsik/query/booking/repository/projection/TimeRange.java
+++ query-service/src/main/java/com/github/matsik/query/booking/repository/projection/TimeRange.java
@@ -7,5 +7,9 @@ public record TimeRange(int start, int end) {
         public static TimeRange create(Row row) {
             return new TimeRange(row.getInt("start"), row.getInt("end"));
         }
+
+        public static TimeRange create(int start, int end) {
+            return new TimeRange(start, end);
+        }
     }
 }
--- query-service/src/test/java/com/github/matsik/query/booking/service/BookingServiceTest.java
+++ query-service/src/test/java/com/github/matsik/query/booking/service/BookingServiceTest.java
@@ -1,13 +1,22 @@
 package com.github.matsik.query.booking.service;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.BatchStatement;
+import com.datastax.oss.driver.api.core.cql.BatchType;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.github.matsik.cassandra.model.BookingByServiceAndDate;
+import com.github.matsik.cassandra.model.BookingByUser;
 import com.github.matsik.cassandra.model.BookingPartitionKey;
 import com.github.matsik.query.booking.query.GetAvailableTimeRangesQuery;
+import com.github.matsik.query.booking.repository.projection.TimeRange;
 import com.github.matsik.query.config.cassandra.client.CassandraClientConfiguration;
 import com.github.matsik.query.config.cassandra.client.CassandraClientProperties;
 import com.github.matsik.query.config.cassandra.mapper.booking.BookingMapperConfiguration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,7 +33,11 @@ import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = {
         BookingServiceTest.TestCassandraConfig.class,
@@ -51,6 +64,9 @@ class BookingServiceTest {
     }
 
     @Autowired
+    private CqlSession cqlSession;
+
+    @Autowired
     private BookingService service;
 
     @BeforeAll
@@ -58,11 +74,201 @@ class BookingServiceTest {
         execMigration();
     }
 
-    @Test
-    void getAvailableTimeRanges() {
-        BookingPartitionKey key = BookingPartitionKey.Factory.create(LocalDate.of(2025, 10, 1), UUID.randomUUID());
-        GetAvailableTimeRangesQuery query = new GetAvailableTimeRangesQuery(key, 30);
-        service.getAvailableTimeRanges(query);
+    @AfterEach
+    void tearDown() {
+        clearBookingsTable();
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideGetAvailableTimeRangesTestCases")
+    void getAvailableTimeRanges(
+            String name,
+            List<Booking> preTestState,
+            GetAvailableTimeRangesQuery query,
+            List<TimeRange> expected
+    ) {
+        // given
+        preTestState.forEach(this::persistBooking);
+
+        // when
+        List<TimeRange> result = service.getAvailableTimeRanges(query);
+
+        // then
+        assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> provideGetAvailableTimeRangesTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        "Should generate all possible booking dates for empty day",
+                        List.of(
+                        ),
+                        getAvailableTimeRangesQuery(120),
+                        List.of(
+                                TimeRange.Factory.create(0, 120),
+                                TimeRange.Factory.create(135, 255),
+                                TimeRange.Factory.create(270, 390),
+                                TimeRange.Factory.create(405, 525),
+                                TimeRange.Factory.create(540, 660),
+                                TimeRange.Factory.create(675, 795),
+                                TimeRange.Factory.create(810, 930),
+                                TimeRange.Factory.create(945, 1065),
+                                TimeRange.Factory.create(1080, 1200),
+                                TimeRange.Factory.create(1215, 1335)
+                        )
+                ),
+                Arguments.of(
+                        "Should find no available time range if all are occupied",
+                        List.of(
+                                booking(0, 60),
+                                booking(75, 135),
+                                booking(150, 210),
+                                booking(225, 285),
+                                booking(300, 360),
+                                booking(375, 435),
+                                booking(450, 510),
+                                booking(525, 585),
+                                booking(600, 660),
+                                booking(675, 735),
+                                booking(750, 810),
+                                booking(825, 885),
+                                booking(900, 960),
+                                booking(975, 1035),
+                                booking(1050, 1110),
+                                booking(1125, 1185),
+                                booking(1200, 1260),
+                                booking(1275, 1335),
+                                booking(1350, 1410)
+                        ),
+                        getAvailableTimeRangesQuery(60),
+                        List.of(
+                        )
+                ),
+                Arguments.of(
+                        "Should round 100 to 120 and find the single available time range",
+                        List.of(
+                                booking(0, 660),
+                                booking(810, 1335)
+                        ),
+                        getAvailableTimeRangesQuery(100),
+                        List.of(
+                                TimeRange.Factory.create(675, 795)
+                        )
+                ),
+                Arguments.of(
+                        "Should find available time ranges in heterogeneous time ranges",
+                        List.of(
+                                booking(0, 120),
+                                booking(270, 660),
+                                booking(945, 1200)
+                        ),
+                        getAvailableTimeRangesQuery(45),
+                        List.of(
+                                TimeRange.Factory.create(135, 255),
+                                TimeRange.Factory.create(675, 795),
+                                TimeRange.Factory.create(810, 930),
+                                TimeRange.Factory.create(1215, 1335)
+                        )
+                )
+        );
+    }
+
+    private static Booking booking(int start, int end) {
+        BookingPartitionKey key = aBookingPartitionKey();
+        return newBooking(key.serviceId(), key.date(), UUID.randomUUID(), start, end);
+    }
+
+    private static GetAvailableTimeRangesQuery getAvailableTimeRangesQuery(int duration) {
+        BookingPartitionKey key = aBookingPartitionKey();
+        return GetAvailableTimeRangesQuery.Factory.create(key.date(), key.serviceId(), duration);
+    }
+
+    private static BookingPartitionKey aBookingPartitionKey() {
+        return BookingPartitionKey.Factory.create(numberToLocalDate(1), numberToUUID(1));
+    }
+
+    private static Booking newBooking(UUID serviceId, LocalDate date, UUID bookingId, int start, int end) {
+        BookingByServiceAndDate bookingByServiceAndDate = BookingByServiceAndDate.builder()
+                .serviceId(serviceId)
+                .date(date)
+                .bookingId(bookingId)
+                .userId(userId())
+                .start(start)
+                .end(end)
+                .build();
+
+        BookingByUser bookingByUser = BookingByUser.builder()
+                .userId(userId())
+                .serviceId(serviceId)
+                .date(date)
+                .bookingId(bookingId)
+                .start(start)
+                .end(end)
+                .build();
+
+        return new Booking(
+                bookingByServiceAndDate,
+                bookingByUser
+        );
+    }
+
+    private void clearBookingsTable() {
+        cqlSession.execute("TRUNCATE booking_system.bookings_by_service_and_date");
+        cqlSession.execute("TRUNCATE booking_system.bookings_by_user");
+    }
+
+    private void persistBooking(Booking booking) {
+        BookingByServiceAndDate bookingByServiceAndDate = booking.bookingByServiceAndDate();
+
+        BoundStatement insertBookingServiceAndDate = cqlSession.prepare(
+                "INSERT INTO booking_system.bookings_by_service_and_date " +
+                        "(service_id, date, booking_id, user_id, start, end) " +
+                        "VALUES (?, ?, ?, ?, ?, ?)"
+        ).bind(
+                bookingByServiceAndDate.serviceId(),
+                bookingByServiceAndDate.date(),
+                bookingByServiceAndDate.bookingId(),
+                bookingByServiceAndDate.userId(),
+                bookingByServiceAndDate.start(),
+                bookingByServiceAndDate.end()
+        );
+
+        BookingByUser insertBookingByUser = booking.bookingByUser();
+        BoundStatement insertUser = cqlSession.prepare(
+                "INSERT INTO booking_system.bookings_by_user " +
+                        "(user_id, service_id, date, booking_id, start, end) " +
+                        "VALUES (?, ?, ?, ?, ?, ?)"
+        ).bind(
+                insertBookingByUser.userId(),
+                insertBookingByUser.serviceId(),
+                insertBookingByUser.date(),
+                insertBookingByUser.bookingId(),
+                insertBookingByUser.start(),
+                insertBookingByUser.end()
+        );
+
+        BatchStatement batch = BatchStatement.builder(BatchType.LOGGED)
+                .addStatement(insertBookingServiceAndDate)
+                .addStatement(insertUser)
+                .build();
+
+        cqlSession.execute(batch);
+    }
+
+    private record Booking(BookingByServiceAndDate bookingByServiceAndDate, BookingByUser bookingByUser) {
+    }
+
+    private static UUID userId() {
+        return numberToUUID(1);
+    }
+
+    public static UUID numberToUUID(long number) {
+        String uuidString = String.format("%08d-0000-0000-0000-000000000000", number);
+        return UUID.fromString(uuidString);
+    }
+
+    public static LocalDate numberToLocalDate(int number) {
+        return LocalDate.of(2025, 9, number);
     }
 
     private static void execMigration() throws IOException {
