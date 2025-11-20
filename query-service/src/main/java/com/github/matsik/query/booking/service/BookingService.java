3acba3af18ba6d6f87a152f2d36172b5756e1a97
Mateusz <siiikor.sikorski@gmail.com>
2025-11-20 13:47:32 +0100

    Add span attributes for query methods

--- query-service/src/main/java/com/github/matsik/query/booking/service/BookingService.java
+++ query-service/src/main/java/com/github/matsik/query/booking/service/BookingService.java
@@ -1,15 +1,17 @@
 package com.github.matsik.query.booking.service;
 
 import com.github.matsik.dto.BookingPartitionKey;
+import com.github.matsik.dto.TimeRange;
 import com.github.matsik.query.booking.query.GetAvailableTimeRangesQuery;
 import com.github.matsik.query.booking.query.GetFirstUserBookingsQuery;
 import com.github.matsik.query.booking.query.GetNextUserBookingsQuery;
 import com.github.matsik.query.booking.query.GetUserBookingQuery;
 import com.github.matsik.query.booking.query.GetUserBookingsQuery;
 import com.github.matsik.query.booking.repository.BookingRepository;
-import com.github.matsik.dto.TimeRange;
 import com.github.matsik.query.booking.repository.projection.UserBooking;
 import com.github.matsik.query.booking.service.exception.UserBookingNotFoundException;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,9 @@ public class BookingService {
 
     @WithSpan(kind = SpanKind.SERVER)
     public List<TimeRange> getAvailableTimeRanges(GetAvailableTimeRangesQuery query) {
+        Span span = Span.current();
+        setSpanAttributes(span, query);
+
         BookingPartitionKey key = query.bookingPartitionKey();
         List<TimeRange> unavailableTimeRanges = repository.getBookedTimeRanges(key.serviceId(), key.date());
 
@@ -34,12 +39,32 @@ public class BookingService {
         return availableTimeRangesCalculator.getAvailableTimeRanges(unavailableTimeRanges, serviceDuration);
     }
 
+    private void setSpanAttributes(Span span, GetAvailableTimeRangesQuery query) {
+        BookingPartitionKey bookingPartitionKey = query.bookingPartitionKey();
+
+        span.setAttribute(AttributeKey.stringKey("getAvailableTimeRangesQuery.bookingPartitionKey.serviceId"), bookingPartitionKey.serviceId().toString());
+        span.setAttribute(AttributeKey.stringKey("getAvailableTimeRangesQuery.bookingPartitionKey.date"), bookingPartitionKey.date().toString());
+        span.setAttribute(AttributeKey.longKey("getAvailableTimeRangesQuery.serviceDuration"), query.serviceDuration());
+    }
+
     @WithSpan(kind = SpanKind.SERVER)
     public TimeRange getUserBookingTimeRange(GetUserBookingQuery query) {
+        Span span = Span.current();
+        setSpanAttributes(span, query);
+
         BookingPartitionKey key = query.bookingPartitionKey();
 
         return repository.getUserBookingTimeRange(query.userId(), key.serviceId(), key.date(), query.bookingId())
                 .orElseThrow(() -> new UserBookingNotFoundException(query));
+    }
+
+    private void setSpanAttributes(Span span, GetUserBookingQuery query) {
+        BookingPartitionKey bookingPartitionKey = query.bookingPartitionKey();
+
+        span.setAttribute(AttributeKey.stringKey("getUserBookingQuery.bookingPartitionKey.serviceId"), bookingPartitionKey.serviceId().toString());
+        span.setAttribute(AttributeKey.stringKey("getUserBookingQuery.bookingPartitionKey.date"), bookingPartitionKey.date().toString());
+        span.setAttribute(AttributeKey.stringKey("getUserBookingQuery.userId"), query.userId().toString());
+        span.setAttribute(AttributeKey.stringKey("getUserBookingQuery.bookingId"), query.bookingId().toString());
     }
 
     public List<UserBooking> getUserBookings(GetUserBookingsQuery query) {
@@ -51,11 +76,22 @@ public class BookingService {
 
     @WithSpan(kind = SpanKind.SERVER)
     private List<UserBooking> getFirstUserBookings(GetFirstUserBookingsQuery query) {
+        Span span = Span.current();
+        setSpanAttributes(span, query);
+
         return repository.getFirstUserBookings(query.userId(), query.limit());
+    }
+
+    private void setSpanAttributes(Span span, GetFirstUserBookingsQuery query) {
+        span.setAttribute(AttributeKey.stringKey("getFirstUserBookingsQuery.userId"), query.userId().toString());
+        span.setAttribute(AttributeKey.longKey("getFirstUserBookingsQuery.limit"), query.limit());
     }
 
     @WithSpan(kind = SpanKind.SERVER)
     private List<UserBooking> getNextUserBookings(GetNextUserBookingsQuery query) {
+        Span span = Span.current();
+        setSpanAttributes(span, query);
+
         return repository.getNextUserBookings(
                 query.userId(),
                 query.cursorServiceId(),
@@ -65,4 +101,11 @@ public class BookingService {
         );
     }
 
+    private void setSpanAttributes(Span span, GetNextUserBookingsQuery query) {
+        span.setAttribute(AttributeKey.stringKey("getNextUserBookingsQuery.userId"), query.userId().toString());
+        span.setAttribute(AttributeKey.stringKey("getNextUserBookingsQuery.cursorServiceId"), query.cursorServiceId().toString());
+        span.setAttribute(AttributeKey.stringKey("getNextUserBookingsQuery.cursorDate"), query.cursorDate().toString());
+        span.setAttribute(AttributeKey.stringKey("getNextUserBookingsQuery.cursorBookingId"), query.cursorBookingId().toString());
+        span.setAttribute(AttributeKey.longKey("getNextUserBookingsQuery.limit"), query.limit());
+    }
 }
