fe8e092a5d5bfde04921500ac1f0c6846496744d
Mateusz <siiikor.sikorski@gmail.com>
2025-12-08 20:35:15 +0100

    Extract caching code

--- command-service/src/main/java/com/github/matsik/command/booking/repository/BookingPersistenceCachingAdapter.java
+++ command-service/src/main/java/com/github/matsik/command/booking/repository/BookingPersistenceCachingAdapter.java
@@ -5,6 +5,7 @@ import com.github.matsik.cassandra.entity.BookingByServiceAndDate;
 import com.github.matsik.command.booking.service.BookingCommandsPort;
 import com.github.matsik.dto.BookingPartitionKey;
 import com.github.matsik.dto.TimeRange;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -19,40 +20,17 @@ public class BookingPersistenceCachingAdapter implements BookingCommandsPort {
 
     private final BookingPersistenceAdapter bookingPersistenceAdapter;
 
-    private final Map<BookingPartitionKey, Set<Booking>> bookingsCache;
+    private final BookingCache bookingsCache;
 
     public BookingPersistenceCachingAdapter(BookingPersistenceAdapter bookingPersistenceAdapter) {
         this.bookingPersistenceAdapter = bookingPersistenceAdapter;
 
-        this.bookingsCache = new HashMap<>();
-    }
-
-    private void clear() {
-        bookingsCache.clear();
-    }
-
-    private Set<Booking> fetch(BookingPartitionKey bookingPartitionKey) {
-        Set<Booking> bookings = bookingsCache.get(bookingPartitionKey);
-        if (bookings == null) {
-            List<BookingByServiceAndDate> freshBookings = bookingPersistenceAdapter.findAllByServiceAndDate(
-                    bookingPartitionKey.serviceId(),
-                    bookingPartitionKey.date()
-            );
-            load(bookingPartitionKey, freshBookings);
-        }
-        return bookingsCache.get(bookingPartitionKey);
-    }
-
-    private void load(BookingPartitionKey key, List<BookingByServiceAndDate> freshBookings) {
-        Set<Booking> bookings = bookingsCache.computeIfAbsent(key, _ -> new HashSet<>());
-        for (BookingByServiceAndDate booking : freshBookings) {
-            bookings.add(Booking.of(booking));
-        }
+        this.bookingsCache = new BookingCache(new HashMap<>());
     }
 
     @Override
     public long findOverlappingBookingCount(BookingPartitionKey bookingPartitionKey, TimeRange timeRange) {
-        Set<Booking> bookings = fetch(bookingPartitionKey);
+        Set<Booking> bookings = bookingsCache.fetch(bookingPartitionKey);
         return bookings.stream()
                 .filter(booking -> isOverlap(booking, timeRange))
                 .count();
@@ -66,19 +44,13 @@ public class BookingPersistenceCachingAdapter implements BookingCommandsPort {
     public UUID createBooking(BookingPartitionKey bookingPartitionKey, UUID userId, TimeRange timeRange) {
         UUID bookingId = bookingPersistenceAdapter.createBooking(bookingPartitionKey, userId, timeRange);
         Booking booking = Booking.of(bookingPartitionKey, bookingId, userId, timeRange);
-        add(booking);
+        bookingsCache.add(booking);
         return bookingId;
-    }
-
-    private void add(Booking booking) {
-        BookingPartitionKey key = BookingPartitionKey.of(booking.serviceId(), booking.date());
-        bookingsCache.computeIfAbsent(key, _ -> new HashSet<>())
-                .add(booking);
     }
 
     @Override
     public Optional<UUID> findBookingOwner(BookingPartitionKey bookingPartitionKey, UUID bookingId) {
-        Set<Booking> bookings = fetch(bookingPartitionKey);
+        Set<Booking> bookings = bookingsCache.fetch(bookingPartitionKey);
         return bookings.stream()
                 .filter(el -> findBy(el, bookingId))
                 .findAny()
@@ -92,18 +64,7 @@ public class BookingPersistenceCachingAdapter implements BookingCommandsPort {
     @Override
     public void deleteBooking(BookingPartitionKey bookingPartitionKey, UUID userId, UUID bookingId) {
         bookingPersistenceAdapter.deleteBooking(bookingPartitionKey, userId, bookingId);
-        delete(bookingPartitionKey, bookingId, userId);
-    }
-
-    private void delete(BookingPartitionKey bookingPartitionKey, UUID bookingId, UUID userId) {
-        Set<Booking> bookings = bookingsCache.get(bookingPartitionKey);
-        if (bookings == null) {
-            return;
-        }
-        bookings.stream()
-                .filter(el -> findBy(el, bookingPartitionKey, bookingId, userId))
-                .findAny()
-                .ifPresent(bookings::remove);
+        bookingsCache.delete(bookingPartitionKey, bookingId, userId);
     }
 
     private boolean findBy(Booking booking, BookingPartitionKey bookingPartitionKey, UUID bookingId, UUID userId) {
@@ -111,6 +72,52 @@ public class BookingPersistenceCachingAdapter implements BookingCommandsPort {
                 Objects.equal(booking.date(), bookingPartitionKey.date()) &&
                 Objects.equal(booking.bookingId(), bookingId) &&
                 Objects.equal(booking.userId(), userId);
+    }
+
+    @RequiredArgsConstructor
+    private class BookingCache {
+
+        private final Map<BookingPartitionKey, Set<Booking>> bookingsCache;
+
+        private void clear() {
+            bookingsCache.clear();
+        }
+
+        private void add(Booking booking) {
+            BookingPartitionKey key = BookingPartitionKey.of(booking.serviceId(), booking.date());
+            bookingsCache.computeIfAbsent(key, _ -> new HashSet<>())
+                    .add(booking);
+        }
+
+        private void delete(BookingPartitionKey bookingPartitionKey, UUID bookingId, UUID userId) {
+            Set<Booking> bookings = bookingsCache.get(bookingPartitionKey);
+            if (bookings == null) {
+                return;
+            }
+            bookings.stream()
+                    .filter(el -> findBy(el, bookingPartitionKey, bookingId, userId))
+                    .findAny()
+                    .ifPresent(bookings::remove);
+        }
+
+        private Set<Booking> fetch(BookingPartitionKey bookingPartitionKey) {
+            Set<Booking> bookings = bookingsCache.get(bookingPartitionKey);
+            if (bookings == null) {
+                List<BookingByServiceAndDate> freshBookings = bookingPersistenceAdapter.findAllByServiceAndDate(
+                        bookingPartitionKey.serviceId(),
+                        bookingPartitionKey.date()
+                );
+                load(bookingPartitionKey, freshBookings);
+            }
+            return bookingsCache.get(bookingPartitionKey);
+        }
+
+        private void load(BookingPartitionKey key, List<BookingByServiceAndDate> freshBookings) {
+            Set<Booking> bookings = bookingsCache.computeIfAbsent(key, _ -> new HashSet<>());
+            for (BookingByServiceAndDate booking : freshBookings) {
+                bookings.add(Booking.of(booking));
+            }
+        }
     }
 
     private record Booking(
